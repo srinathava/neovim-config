4604b3f7c13bdb664297edf02c30c44919aa23e1
Srinath Avadhanula <srinathava@gmail.com>
2022-05-28 11:14:16 -0400

    Update clangd.lua

--- lua/user/lsp/settings/clangd.lua
+++ lua/user/lsp/settings/clangd.lua
@@ -1,7 +1,7 @@
 local lspconfig = require('lspconfig')
 return {
     cmd = {
-        "/mathworks/hub/share/sbtools/external-apps/llvm/llvm-13.0.0/install/deb11-64/bin/clangd",
+        "clangd",
         "--header-insertion=never",
         "--completion-parse=auto",
         "-j=12"
