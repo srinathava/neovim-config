29df0cfe1b06c9b0b116f87bcb380e6756ff2f29
Srinath Avadhanula <savadhan@mathworks.com>
2022-06-21 12:27:17 -0400

    add matlabls server

--- lua/user/lsp/init.lua
+++ lua/user/lsp/init.lua
@@ -1,14 +1,43 @@
 local lspconfig = require('lspconfig')
 local lspinstaller = require('nvim-lsp-installer')
 local handlers = require('user.lsp.handlers')
+local configs = require 'lspconfig.configs'
 
 handlers.setup()
 
-local myservers = {"pyright", "sumneko_lua", "clangd", "emmet_ls", "cssls", "tsserver"}
+local myservers = {"pyright", "sumneko_lua", "clangd", "emmet_ls", "cssls", "tsserver", "matlabls"}
 
 lspinstaller.setup {
     ensure_installed = myservers
 }
+
+if not configs.matlabls then
+    configs.matlabls = {
+        default_config = {
+            cmd = {
+                "node",
+                "/mathworks/inside/labs/dev/matlab_coder_tools/matlabls/matlabls/server/out/main.js",
+                "--matlabLaunchCmdArgs=-nodisplay",
+                "--stdio"
+            },
+            filetypes = {'matlab'},
+            root_dir = lspconfig.util.root_pattern('mw_anchor'),
+            single_file_support = false,
+        },
+        commands = {},
+        docs = {
+            description = [[
+            Experimental matlabls server
+            ]],
+            default_config = {
+                root_dir = [[
+                root_pattern('mw_anchor')
+                ]],
+                capabilities = [[default capabilities]],
+            },
+        }
+    }
+end
 
 for _, name in pairs(myservers) do
     local common_opts = {
